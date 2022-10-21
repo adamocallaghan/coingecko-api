69e5bb982a516cef2f2882f402b78de57394e749
adamocallaghan <adam.ocallaghan@yahoo.ie>
2022-10-21 16:11:22 +0100

    server & mongo working, passing params still req'd

--- mongotest.js
+++ mongotest.js
@@ -20,22 +20,23 @@ const connectMongoCloud = async() => {
   });
 }
 
-// connectMongoCloud();
+connectMongoCloud();
 
-const saveCoin = async (request, response) => {
+const saveCoin = async (theCoin) => {
 
-  const coin = new Coin({
-    "coinId":"newdublin",
-    "coinPrices_Object":{
-      "Day 0":{"todaysPrice":"0.0766","todaysDate":"2022-10-17T00:00:00.000Z","todaysDateMilliseconds":1665964800000,"todaysPriceLong":0.07662291659226893},
-      "Day 1":{"todaysPrice":"0.0753","todaysDate":"2022-10-18T00:00:00.000Z","todaysDateMilliseconds":1666051200000,"todaysPriceLong":0.07534350952689779},
-      "Day 2":{"todaysPrice":"0.0726","todaysDate":"2022-10-19T00:00:00.000Z","todaysDateMilliseconds":1666137600000,"todaysPriceLong":0.07263690517706178},
-      "Day 3":{"todaysPrice":"0.0832","todaysDate":"2022-10-20T00:00:00.000Z","todaysDateMilliseconds":1666224000000,"todaysPriceLong":0.08320276019215789},
-      "Day 4":{"todaysPrice":"0.0775","todaysDate":"2022-10-21T00:00:00.000Z","todaysDateMilliseconds":1666310400000,"todaysPriceLong":0.07751429281678977},
-      "Day 5":{"todaysPrice":"0.0774","todaysDate":"2022-10-21T11:13:38.000Z","todaysDateMilliseconds":1666350818000,"todaysPriceLong":0.07737231837606312}
-    }
-    }
-  )
+  // const theCoin = {
+  //   "coinId":"blerbugh",
+  //   "coinPrices_Object":{
+  //     "Day 0":{"todaysPrice":"0.0766","todaysDate":"2022-10-17T00:00:00.000Z","todaysDateMilliseconds":1665964800000,"todaysPriceLong":0.07662291659226893},
+  //     "Day 1":{"todaysPrice":"0.0753","todaysDate":"2022-10-18T00:00:00.000Z","todaysDateMilliseconds":1666051200000,"todaysPriceLong":0.07534350952689779},
+  //     "Day 2":{"todaysPrice":"0.0726","todaysDate":"2022-10-19T00:00:00.000Z","todaysDateMilliseconds":1666137600000,"todaysPriceLong":0.07263690517706178},
+  //     "Day 3":{"todaysPrice":"0.0832","todaysDate":"2022-10-20T00:00:00.000Z","todaysDateMilliseconds":1666224000000,"todaysPriceLong":0.08320276019215789},
+  //     "Day 4":{"todaysPrice":"0.0775","todaysDate":"2022-10-21T00:00:00.000Z","todaysDateMilliseconds":1666310400000,"todaysPriceLong":0.07751429281678977},
+  //     "Day 5":{"todaysPrice":"0.0774","todaysDate":"2022-10-21T11:13:38.000Z","todaysDateMilliseconds":1666350818000,"todaysPriceLong":0.07737231837606312}
+  //   }
+  //   }
+
+  const coin = new Coin(theCoin);
 
   try {
     await coin.save();
@@ -45,7 +46,22 @@ const saveCoin = async (request, response) => {
   }
 };
 
-// saveCoin();
+const saveTheCoin = {
+  "coinId":"murtilia",
+  "coinPrices_Object":{
+    "Day 0":{"todaysPrice":"0.0766","todaysDate":"2022-10-17T00:00:00.000Z","todaysDateMilliseconds":1665964800000,"todaysPriceLong":0.07662291659226893},
+    "Day 1":{"todaysPrice":"0.0753","todaysDate":"2022-10-18T00:00:00.000Z","todaysDateMilliseconds":1666051200000,"todaysPriceLong":0.07534350952689779},
+    "Day 2":{"todaysPrice":"0.0726","todaysDate":"2022-10-19T00:00:00.000Z","todaysDateMilliseconds":1666137600000,"todaysPriceLong":0.07263690517706178},
+    "Day 3":{"todaysPrice":"0.0832","todaysDate":"2022-10-20T00:00:00.000Z","todaysDateMilliseconds":1666224000000,"todaysPriceLong":0.08320276019215789},
+    "Day 4":{"todaysPrice":"0.0775","todaysDate":"2022-10-21T00:00:00.000Z","todaysDateMilliseconds":1666310400000,"todaysPriceLong":0.07751429281678977},
+    "Day 5":{"todaysPrice":"0.0774","todaysDate":"2022-10-21T11:13:38.000Z","todaysDateMilliseconds":1666350818000,"todaysPriceLong":0.07737231837606312}
+  }
+  }
 
-export const connectMongoCloudFunction = connectMongoCloud();
-export const saveCoinFunction = saveCoin();
+saveCoin(saveTheCoin);
+
+export { connectMongoCloud };
+export { saveCoin };
+
+// export const connectMongoCloudFunction = connectMongoCloud();
+// export const saveCoinFunction = saveCoin();
